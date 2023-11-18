e3b997bff441901b3ca21678cdf0714fe4377938
Sergey <asaulsergey@gmail.com>
2023-11-18 21:58:09 +0300

    the end

--- source/js/modules/slider-jury.js
+++ source/js/modules/slider-jury.js
@@ -26,14 +26,12 @@ const initSwiperJury = () => {
         slidesPerView: 4, spaceBetween: 40,
       },
 
-      1120: {
-        slidesPerView: 3, spaceBetween: 70,
+      768: {
+        slidesPerView: 2, spaceBetween: 32, initialSlide: 2
       },
 
-      768: {
-        slidesPerView: 2, spaceBetween: 32,
-      }, 0: {
-        slidesPerView: 1, spaceBetween: 32,
+      320: {
+        slidesPerView: 1, spaceBetween: 32, initialSlide: 2
       },
     },
 
