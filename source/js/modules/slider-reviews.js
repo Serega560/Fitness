d018ca1713860a75a9ced4194c05dd702540d223
Sergey <asaulsergey@gmail.com>
2023-11-26 00:37:46 +0300

    C:/Program Files/Git/

--- source/js/modules/slider-reviews.js
+++ source/js/modules/slider-reviews.js
@@ -1,8 +1,6 @@
 const swiperReviews = document.querySelector('[data-swiper="reviews"]');
 const swiperButtonPrev = document.querySelector('[data-swiper-button-prev="reviews"]');
 const swiperButtonNext = document.querySelector('[data-swiper-button-next="reviews"]');
-console.log(swiperButtonPrev);
-console.log(swiperButtonNext)
 //
 //
 const initSwiperReviews = () => {
